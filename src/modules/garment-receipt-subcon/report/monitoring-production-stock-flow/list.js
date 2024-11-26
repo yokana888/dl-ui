846b79c376418c79e23fc3eeca74df9e2f04c147
achensetyawati <achenzetya@gmail.com>
2024-11-26 17:05:44 +0700

    update Receipt Subcon monitoring unit GMT

--- src/modules/garment-receipt-subcon/report/monitoring-production-stock-flow/list.js
+++ src/modules/garment-receipt-subcon/report/monitoring-production-stock-flow/list.js
@@ -12,6 +12,7 @@ export class List {
   }
   bind(context) {
     this.context = context;
+    this.UnitItem = "GARMENT";
   }
 
   controlOptions = {
@@ -25,14 +26,9 @@ export class List {
 
   @bindable UnitItem;
 
-  UnitItems = [
-    "",
-    "KONFEKSI 2A",
-    "KONFEKSI 2B",
-    "KONFEKSI 2C",
-    "KONFEKSI 1A",
-    "KONFEKSI 1B",
-  ];
+  // UnitItems = [
+  //   "GARMENT"
+  // ];
 
   searching() {
     this.error = {};
@@ -1193,33 +1189,5 @@ export class List {
     this.ro = null;
     this.date = null;
     this.unit = null;
-  }
-
-  UnitItemChanged(newvalue) {
-    // console.log(newvalue);
-    if (newvalue) {
-      if (newvalue === "KONFEKSI 2A") {
-        this.unit = 45;
-        this.unitname = "KONFEKSI 2A";
-      } else if (newvalue === "KONFEKSI 2B") {
-        this.unit = 46;
-        this.unitname = "KONFEKSI 2B";
-      } else if (newvalue === "KONFEKSI 2C") {
-        this.unit = 47;
-        this.unitname = "KONFEKSI 2C";
-      } else if (newvalue === "KONFEKSI 1A") {
-        this.unit = 51;
-        this.unitname = "KONFEKSI 1A";
-      } else if (newvalue === "KONFEKSI 1B") {
-        this.unit = 52;
-        this.unitname = "KONFEKSI 1B";
-      } else {
-        this.unit = 0;
-        this.unitname = "";
-      }
-    } else {
-      this.unit = 0;
-      this.unitname = "";
-    }
   }
 }
